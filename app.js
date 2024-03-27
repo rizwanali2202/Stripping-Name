4894b330d76b2b8318d06ff95dc456008c03e7ef
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-27 17:54:08 +0500

    Here is Stripping Name case file
    in this program you can make name in lower case, upper case and title case

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let personName = `\n\t rizwan ali \n\t`;
+console.log(personName);
+let trimmed = (personName.trim());
+console.log(trimmed);
+// \n is use for next line
+// \t is use for four spaces like as tab
+// trim kasi b function ke start ke spaces ko khatam karnay k laya use hota ha
---
+++ app.ts
@@ -0,0 +1,8 @@
+let personName : string = `\n\t rizwan ali \n\t`;
+console.log(personName);
+let trimmed : string = (personName.trim());
+console.log(trimmed);
+
+// \n is use for next line
+// \t is use for four spaces like as tab
+// trim kasi b function ke start ke spaces ko khatam karnay k laya use hota ha
